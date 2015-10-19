9d17436a3bcb1ef0c394131a31ef02c8e3c06948
baloran <contact@baloran.fr>
2015-10-19 22:45:06 +0200

    fix fatal error

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ add_theme_support('post-thumbnails');
 add_image_size( 'front_image', 300, 9999 );
 
 add_action( 'init', 'create_posttype' );
-add_action( 'init', 'people_init' );
 
 function create_posttype() {
   register_post_type( 'acme_product',
@@ -16,21 +15,6 @@ function create_posttype() {
       'public' => true,
       'has_archive' => true,
       'rewrite' => array('slug' => 'products'),
-    )
-  );
-}
-
-function people_init() {
-  // create a new taxonomy
-  register_taxonomy(
-    'post',
-    array(
-      'label' => __( 'People' ),
-      'rewrite' => array( 'slug' => 'person' ),
-      'capabilities' => array(
-        'assign_terms' => 'edit_guides',
-        'edit_terms' => 'publish_guides'
-      )
     )
   );
 }
