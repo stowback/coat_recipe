bd438e848f4af3f0c6f7586e8b7d92f0e8f79e0e
baloran <contact@baloran.fr>
2015-10-20 14:42:54 +0200

    udpate comments

--- functions.php
+++ functions.php
@@ -5,6 +5,11 @@ add_image_size( 'front_image', 300, 9999 );
 
 include_once('vendors/advanced-custom-fields/acf.php');
 
+// Vetements
 include_once('core/coat.php');
+
+// Recettes de vétements
 include_once('core/recipe.php');
+
+// Relation between that
 include_once('core/relation.php');
