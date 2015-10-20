691910afd2bc7c372170c387aebd16fb1539b973
baloran <contact@baloran.fr>
2015-10-20 10:47:56 +0200

    add recette custom post type

--- functions.php
+++ functions.php
@@ -72,3 +72,21 @@ function create_term_type_coat() {
     Recette
   
 *************************************************************/
+
+add_action( 'init', 'create_recette' );
+
+// Create recette
+function create_recette () {
+  register_post_type( 'recette',
+    array(
+      'labels' => array(
+        'name' => __( 'Recettes' ),
+        'singular_name' => __( 'recette' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'taxonomies' => array(),
+      'rewrite' => array('slug' => 'recettes'),
+    )
+  );
+}
