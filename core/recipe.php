68c368cef75f4c956deb28713df3bcb82050d5b4
baloran <contact@baloran.fr>
2015-10-21 11:29:28 +0200

    add tags for recette

--- core/recipe.php
+++ core/recipe.php
@@ -7,9 +7,11 @@
 **************************************************************/
 
 add_action( 'init', 'create_recette' );
+add_action( 'init', 'create_tags_recette' );
+add_action( 'init', 'create_the_tags_recette' );
 
 // Create recette
-function create_recette () {
+function create_recette() {
   register_post_type( 'recette',
     array(
       'labels' => array(
@@ -18,8 +20,40 @@ function create_recette () {
       ),
       'public' => true,
       'has_archive' => true,
-      'taxonomies' => array(),
+      'taxonomies' => array('theme'),
       'rewrite' => array('slug' => 'recettes'),
+    )
+  );
+}
+
+function create_tags_recette() {
+  register_taxonomy(
+    'theme',
+    'recette',
+    array(
+      'label' => __( 'theme' ),
+      'rewrite' => array( 'slug' => 'theme' ),
+      'hierarchical' => false,
+    )
+  );
+}
+
+function create_the_tags_recette () {
+  wp_insert_term(
+    'bleu', // the term 
+    'theme', // the taxonomy
+    array(
+      'description'=> 'Bleu',
+      'slug' => 'bleu'
+    )
+  );
+
+  wp_insert_term(
+    'soirée', // the term 
+    'theme', // the taxonomy
+    array(
+      'description'=> 'Soirée',
+      'slug' => 'soiree'
     )
   );
 }
